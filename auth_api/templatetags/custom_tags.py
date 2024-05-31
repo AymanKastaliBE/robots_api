925f133d5abc5181b3f53fd56019662743f1763b
Ayman Kastali <facephi.booth@gmail.com>
2024-05-31 16:33:05 +0400

    custom tag user.group

---
+++ auth_api/templatetags/custom_tags.py
@@ -0,0 +1,10 @@
+# custom_tags.py
+
+from django import template
+
+register = template.Library()
+
+@register.filter(name='user_in_group')
+def user_in_group(user, group_names):
+    groups = group_names.split(',')
+    return user.groups.filter(name__in=groups).exists()
